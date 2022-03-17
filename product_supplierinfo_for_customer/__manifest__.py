de6f0a3c7a3a22041cb8c99007d1415b0f48f714
hea-bmya <hector@bmya.cl>
2022-03-17 16:24:24 -0300

    [FIX] No se elimina configuracion de reemplazo, se filtra por compañia

--- product_supplierinfo_for_customer/__manifest__.py
+++ product_supplierinfo_for_customer/__manifest__.py
@@ -14,6 +14,7 @@
     "depends": ["product", "sales_team"],
     "data": [
         "security/ir.model.access.csv",
+        "security/ir_rule.xml",
         "views/product_pricelist_views.xml",
         "views/product_view.xml",
     ],
